c1306d40892971a242231ee6d3bcea72702b909c
LogicEu <eugenioarteagaa@gmail.com>
2023-08-15 14:12:44 -0400

    Added mat4 structure and implementation, perspectives, rotations and more.

--- spxmath.h
+++ spxmath.h
@@ -81,9 +81,9 @@ typedef struct vec4 {
     float x, y, z, w;
 } vec4;
 
-typedef float mat4[4][4];
-typedef float mat3[3][3];
-typedef float mat2[2][2];
+typedef struct mat4 {
+    float data[4][4];
+} mat4;
 
 float randf(void);
 float absf(float n);
@@ -149,6 +149,24 @@ float vec4_mag(vec4 p);
 float vec4_sqdist(vec4 p, vec4 q);
 float vec4_dist(vec4 p, vec4 q);
 float vec4_dot(vec4 p, vec4 q);
+vec4 vec4_mult_mat4(vec4 p, mat4 m);
+
+mat4 mat4_id(void);
+mat4 mat4_zero(void);
+mat4 mat4_translate(mat4 m, vec3 p);
+mat4 mat4_mult(mat4 m1, mat4 m2);
+mat4 mat4_mult_vec4(mat4 m, vec4 v);
+mat4 mat4_mult_vec3(mat4 m, vec3 v);
+mat4 mat4_scale(mat4 m, vec3 v);
+mat4 mat4_rot(mat4 m, float deg, vec3 rot_axis);
+mat4 mat4_perspective_RH(float fov, float aspect, float near, float far);
+mat4 mat4_perspective_LH(float fov, float aspect, float near, float far);
+mat4 mat4_look_at_RH(vec3 eye_position, vec3 eye_direction, vec3 eye_up);
+mat4 mat4_look_at_LH(vec3 eye_position, vec3 eye_direction, vec3 eye_up);
+mat4 mat4_ortho(float left, float right, float bottom, float top);
+mat4 mat4_perspective(float fov, float aspect, float near, float far);
+mat4 mat4_look_at(vec3 eye_position, vec3 eye_direction, vec3 eye_up);
+mat4 mat4_model(vec3 translation, vec3 scale, vec3 rot_axis, float rot_degs);
 
 vec2 vec2_from_ivec2(ivec2 p);
 vec3 vec3_from_ivec3(ivec3 p);
@@ -616,6 +634,250 @@ float vec4_dist(vec4 p, vec4 q)
 float vec4_dot(vec4 p, vec4 q)
 {
     return p.x * q.x + p.y * q.y + p.z * q.z + p.w * q.w;
+}
+
+vec4 vec4_mult_mat4(vec4 p, mat4 m)
+{
+    vec4 q;
+    q.x = p.x * m.data[0][0] + p.y * m.data[1][0] + p.z * m.data[2][0] + p.w * m.data[3][0];
+    q.y = p.x * m.data[0][1] + p.y * m.data[1][1] + p.z * m.data[2][1] + p.w * m.data[3][1];
+    q.z = p.x * m.data[0][2] + p.y * m.data[1][2] + p.z * m.data[2][2] + p.w * m.data[3][2];
+    q.w = p.x * m.data[0][3] + p.y * m.data[1][3] + p.z * m.data[2][3] + p.w * m.data[3][3];
+    return q;
+}
+
+/* 4 x 4 matrix operations */
+
+mat4 mat4_id(void)
+{
+    mat4 m = {{
+        {1.0F, 0.0F, 0.0F, 0.0F},
+        {0.0F, 1.0F, 0.0F, 0.0F},
+        {0.0F, 0.0F, 1.0F, 0.0F},
+        {0.0F, 0.0F, 0.0F, 1.0F}
+    }};
+    return m;
+}
+
+mat4 mat4_zero(void)
+{
+    mat4 m = {{{0.0F}}};
+    return m;
+}
+
+mat4 mat4_translate(mat4 m, vec3 p)
+{
+    m.data[3][0] = p.x;
+    m.data[3][1] = p.y;
+    m.data[3][2] = p.z;
+    return m;
+}
+
+mat4 mat4_mult(mat4 m1, mat4 m2)
+{
+    mat4 m;
+    m.data[0][0] = m1.data[0][0] * m2.data[0][0] + m1.data[1][0] * m2.data[0][1] + m1.data[2][0] * m2.data[0][2] + m1.data[3][0] * m2.data[0][3];
+    m.data[0][1] = m1.data[0][1] * m2.data[0][0] + m1.data[1][1] * m2.data[0][1] + m1.data[2][1] * m2.data[0][2] + m1.data[3][1] * m2.data[0][3];
+    m.data[0][2] = m1.data[0][2] * m2.data[0][0] + m1.data[1][2] * m2.data[0][1] + m1.data[2][2] * m2.data[0][2] + m1.data[3][2] * m2.data[0][3];
+    m.data[0][3] = m1.data[0][3] * m2.data[0][0] + m1.data[1][3] * m2.data[0][1] + m1.data[2][3] * m2.data[0][2] + m1.data[3][3] * m2.data[0][3];
+    m.data[1][0] = m1.data[0][0] * m2.data[1][0] + m1.data[1][0] * m2.data[1][1] + m1.data[2][0] * m2.data[1][2] + m1.data[3][0] * m2.data[1][3];
+    m.data[1][1] = m1.data[0][1] * m2.data[1][0] + m1.data[1][1] * m2.data[1][1] + m1.data[2][1] * m2.data[1][2] + m1.data[3][1] * m2.data[1][3];
+    m.data[1][2] = m1.data[0][2] * m2.data[1][0] + m1.data[1][2] * m2.data[1][1] + m1.data[2][2] * m2.data[1][2] + m1.data[3][2] * m2.data[1][3];
+    m.data[1][3] = m1.data[0][3] * m2.data[1][0] + m1.data[1][3] * m2.data[1][1] + m1.data[2][3] * m2.data[1][2] + m1.data[3][3] * m2.data[1][3];
+    m.data[2][0] = m1.data[0][0] * m2.data[2][0] + m1.data[1][0] * m2.data[2][1] + m1.data[2][0] * m2.data[2][2] + m1.data[3][0] * m2.data[2][3];
+    m.data[2][1] = m1.data[0][1] * m2.data[2][0] + m1.data[1][1] * m2.data[2][1] + m1.data[2][1] * m2.data[2][2] + m1.data[3][1] * m2.data[2][3];
+    m.data[2][2] = m1.data[0][2] * m2.data[2][0] + m1.data[1][2] * m2.data[2][1] + m1.data[2][2] * m2.data[2][2] + m1.data[3][2] * m2.data[2][3];
+    m.data[2][3] = m1.data[0][3] * m2.data[2][0] + m1.data[1][3] * m2.data[2][1] + m1.data[2][3] * m2.data[2][2] + m1.data[3][3] * m2.data[2][3];
+    m.data[3][0] = m1.data[0][0] * m2.data[3][0] + m1.data[1][0] * m2.data[3][1] + m1.data[2][0] * m2.data[3][2] + m1.data[3][0] * m2.data[3][3];
+    m.data[3][1] = m1.data[0][1] * m2.data[3][0] + m1.data[1][1] * m2.data[3][1] + m1.data[2][1] * m2.data[3][2] + m1.data[3][1] * m2.data[3][3];
+    m.data[3][2] = m1.data[0][2] * m2.data[3][0] + m1.data[1][2] * m2.data[3][1] + m1.data[2][2] * m2.data[3][2] + m1.data[3][2] * m2.data[3][3];
+    m.data[3][3] = m1.data[0][3] * m2.data[3][0] + m1.data[1][3] * m2.data[3][1] + m1.data[2][3] * m2.data[3][2] + m1.data[3][3] * m2.data[3][3];
+    return m;
+}
+
+mat4 mat4_mult_vec4(mat4 m, vec4 p)
+{
+    m.data[0][0] *= p.x;
+    m.data[0][1] *= p.x;
+    m.data[0][2] *= p.x;
+    m.data[0][3] *= p.x;
+    m.data[1][0] *= p.y;
+    m.data[1][1] *= p.y;
+    m.data[1][2] *= p.y;
+    m.data[1][3] *= p.y;
+    m.data[2][0] *= p.z;
+    m.data[2][1] *= p.z;
+    m.data[2][2] *= p.z;
+    m.data[2][3] *= p.z;
+    m.data[3][0] *= p.w;
+    m.data[3][1] *= p.w;
+    m.data[3][2] *= p.w;
+    m.data[3][3] *= p.w;
+    return m;
+}
+
+mat4 mat4_mult_vec3(mat4 m, vec3 p)
+{
+    m.data[0][0] *= p.x;
+    m.data[0][1] *= p.x;
+    m.data[0][2] *= p.x;
+    m.data[0][3] *= p.x;
+    m.data[1][0] *= p.y;
+    m.data[1][1] *= p.y;
+    m.data[1][2] *= p.y;
+    m.data[1][3] *= p.y;
+    m.data[2][0] *= p.z;
+    m.data[2][1] *= p.z;
+    m.data[2][2] *= p.z;
+    m.data[2][3] *= p.z;
+    return m;
+}
+
+mat4 mat4_scale(mat4 m, vec3 p)
+{
+    mat4 M = {{{0.0F}}};
+    M.data[0][0] = p.x;
+    M.data[1][1] = p.y;
+    M.data[2][2] = p.z;
+    M.data[3][3] = 1.0F;
+    return mat4_mult(M, m);
+}
+
+mat4 mat4_rot(mat4 mat, float deg, vec3 rot_axis)
+{
+    float c, s;
+    vec3 axis, temp;
+    mat4 rot = {{{0.0F}}}, m = {{{0.0F}}};
+
+    c = cosf(deg);
+    s = sinf(deg);
+
+    axis = vec3_norm(rot_axis);
+	temp = vec3_mult(axis, 1.0f - c);
+
+    rot.data[0][0] = c + temp.x * axis.x;
+    rot.data[0][1] = temp.x * axis.y + s * axis.z;
+    rot.data[0][2] = temp.x * axis.z - s * axis.y;
+    rot.data[1][0] = temp.y * axis.x - s * axis.z;
+    rot.data[1][1] = c + temp.y * axis.y;
+    rot.data[1][2] = temp.y * axis.z + s * axis.x;
+    rot.data[2][0] = temp.z * axis.x + s * axis.y;
+    rot.data[2][1] = temp.z * axis.y - s * axis.x;
+    rot.data[2][2] = c + temp.z * axis.z;
+
+    m.data[0][0] = mat.data[0][0] * rot.data[0][0] + mat.data[1][0] * rot.data[0][1] + mat.data[2][0] * rot.data[0][2];
+    m.data[0][1] = mat.data[0][1] * rot.data[0][0] + mat.data[1][1] * rot.data[0][1] + mat.data[2][1] * rot.data[0][2];
+    m.data[0][2] = mat.data[0][2] * rot.data[0][0] + mat.data[1][2] * rot.data[0][1] + mat.data[2][2] * rot.data[0][2];
+    m.data[1][0] = mat.data[0][0] * rot.data[1][0] + mat.data[1][0] * rot.data[1][1] + mat.data[2][0] * rot.data[1][2];
+    m.data[1][1] = mat.data[0][1] * rot.data[1][0] + mat.data[1][1] * rot.data[1][1] + mat.data[2][1] * rot.data[1][2];
+    m.data[1][2] = mat.data[0][2] * rot.data[1][0] + mat.data[1][2] * rot.data[1][1] + mat.data[2][2] * rot.data[1][2];
+    m.data[2][0] = mat.data[0][0] * rot.data[2][0] + mat.data[1][0] * rot.data[2][1] + mat.data[2][0] * rot.data[2][2];
+    m.data[2][1] = mat.data[0][1] * rot.data[2][0] + mat.data[1][1] * rot.data[2][1] + mat.data[2][1] * rot.data[2][2];
+    m.data[2][2] = mat.data[0][2] * rot.data[2][0] + mat.data[1][2] * rot.data[2][1] + mat.data[2][2] * rot.data[2][2];
+    m.data[3][0] = mat.data[3][0];
+    m.data[3][1] = mat.data[3][1];
+    m.data[3][2] = mat.data[3][2];
+    m.data[3][3] = mat.data[3][3];
+    return m;
+}
+
+mat4 mat4_perspective_RH(float fov, float aspect, float near, float far)
+{
+    mat4 m = {{{0.0F}}};
+    float tan_half_fov = tanf(fov / 2.0f);
+    m.data[0][0] = 1.0f / (aspect * tan_half_fov);
+    m.data[1][1] = 1.0f / tan_half_fov;
+    m.data[2][2] = (far + near) / (far - near);
+    m.data[2][3] = 1.0f;
+    m.data[3][2] = (2.0f * far * near) / (far - near);
+    return m;
+}
+
+mat4 mat4_perspective_LH(float fov, float aspect, float near, float far)
+{
+    mat4 m = {{{0.0F}}};
+    float tan_half_fov = tanf(fov / 2.0f);
+    m.data[0][0] = 1.0f / (aspect * tan_half_fov);
+    m.data[1][1] = 1.0f / tan_half_fov;
+    m.data[2][2] = (far + near) / (far - near);
+    m.data[2][3] = 1.0f;
+    m.data[3][2] = - (2.0f * far * near) / (far - near);
+    return m;
+}
+
+mat4 mat4_look_at_RH(vec3 eye_position, vec3 eye_direction, vec3 eye_up)
+{
+    vec3 f, s, u;
+    mat4 m = {{{0.0F}}};
+    f = vec3_norm(vec3_sub(eye_direction, eye_position));
+    s = vec3_norm(vec3_cross(f, eye_up));
+    u = vec3_cross(s, f);
+    m.data[0][0] = s.x;
+    m.data[1][0] = s.y;
+    m.data[2][0] = s.z;
+    m.data[0][1] = u.x;
+    m.data[1][1] = u.y;
+    m.data[2][1] = u.z;
+    m.data[0][2] = -f.x;
+    m.data[1][2] = -f.y;
+    m.data[2][2] = -f.z;
+    m.data[3][0] = -vec3_dot(s, eye_position);
+    m.data[3][1] = -vec3_dot(u, eye_position);
+    m.data[3][2] = vec3_dot(f, eye_position);
+    m.data[3][3] = 1.0f;
+    return m;
+}
+
+mat4 mat4_look_at_LH(vec3 eye_position, vec3 eye_direction, vec3 eye_up)
+{
+    vec3 f, s, u;
+    mat4 m = {{{0.0F}}};
+    f = vec3_norm(vec3_sub(eye_direction, eye_position));
+    s = vec3_norm(vec3_cross(eye_up, f));
+    u = vec3_cross(f, s);
+    m.data[0][0] = s.x;
+    m.data[1][0] = s.y;
+    m.data[2][0] = s.z;
+    m.data[0][1] = u.x;
+    m.data[1][1] = u.y;
+    m.data[2][1] = u.z;
+    m.data[0][2] = f.x;
+    m.data[1][2] = f.y;
+    m.data[2][2] = f.z;
+    m.data[3][0] = -vec3_dot(s, eye_position);
+    m.data[3][1] = -vec3_dot(u, eye_position);
+    m.data[3][2] = -vec3_dot(f, eye_position);
+    m.data[3][3] = 1.0f;
+    return m;
+}
+
+mat4 mat4_ortho(float left, float right, float bottom, float top)
+{
+    mat4 ret = mat4_id();
+    ret.data[0][0] = 2.0f / (right - left);
+    ret.data[1][1] = 2.0f / (top - bottom);
+    ret.data[2][2] = - 1.0f;
+    ret.data[3][0] = - (right + left) / (right - left);
+    ret.data[3][1] = - (top + bottom) / (top - bottom);
+    return ret;
+}
+
+mat4 mat4_perspective(float fov, float aspect, float near, float far)
+{
+    return mat4_perspective_RH(fov, aspect, near, far);
+}
+
+mat4 mat4_look_at(vec3 eye_position, vec3 eye_direction, vec3 eye_up)
+{
+    return mat4_look_at_RH(eye_position, eye_direction, eye_up);
+}
+
+mat4 mat4_model(vec3 translation, vec3 scale, vec3 rot_axis, float rot_degs)
+{
+    mat4 model = mat4_scale(mat4_id(), scale);
+    model = mat4_rot(model, rot_degs, rot_axis);
+    model = mat4_translate(model, translation);
+    return model;
 }
 
 /* convert betwen integer and float vector types */
